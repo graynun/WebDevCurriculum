c623523d07ae6168a1cb638d817877295fcde290
graynun <graynun@gmail.com>
2016-06-30 15:11:53 +0900

    Quest07 Reorganized custom events and eventListener

--- Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/skeleton/desktop.js
+++ Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/skeleton/desktop.js
@@ -9,64 +9,57 @@ var Desktop = function() {
 Desktop.prototype.bindEvent = function(){
 	var that = this;
 
-	this.domObj.addEventListener("selectMovingItem", function(){
-		if(event.target.classList[0] === "icon" || event.target.classList[0] === "folder") {
-			for(var i=0; i<that.memberIcons.length;i++) {
-				if(that.memberIcons[i].name === event.target.classList[1]) {
-					that.selectMovingItem(that.memberIcons[i]);
-				}
-			}
-		}else {
-			for(var i=0; i<that.memberWindow.length;i++) {
-				if(that.memberWindow[i].name === event.target.classList[1]) {
-					that.selectMovingItem(that.memberWindow[i]);
-				}
-			}
-		}
-	});
-
 	this.domObj.addEventListener('mousemove', function(event){
 		if(that.movingItem !== null) that.movingItem.movePosition(event);
-	})
-
-	this.domObj.addEventListener("deselectMovingItem", function(){
-		that.movingItem = null;
-	})
-
-	this.domObj.addEventListener("createWindow", function(){
-		for(var i=0; i<that.memberIcons.length;i++){
-			if(that.memberIcons[i].name === event.target.classList[1]){
-				that.createWindow(that.memberIcons[i]);
-			}
-		}
 	})
 }
 
 Desktop.prototype.createIcon = function(name){
 	var newIcon = new Icon(name);
-	newIcon.bindEvent();
-	newIcon.setPosition(200, 400);
-
 	this.memberIcons.push(newIcon);
 	this.domObj.appendChild(newIcon.domObj);
+
+	var that = this;
+
+	newIcon.domObj.addEventListener("selectMovingItem", function(){
+		that.selectMovingItem(newIcon);
+	})
+	newIcon.domObj.addEventListener("deselectMovingItem", function(){
+		that.deselectMovingItem();
+	})
 }
 
 Desktop.prototype.createFolder = function(name){
 	var newFolder = new Folder(name);
-	newFolder.bindEvent();
-	newFolder.setPosition(400, 200);
-
 	this.memberIcons.push(newFolder);
 	this.domObj.appendChild(newFolder.domObj);
+
+	var that = this;
+
+	newFolder.domObj.addEventListener("selectMovingItem", function(){
+		that.selectMovingItem(newFolder);
+	})
+	newFolder.domObj.addEventListener("deselectMovingItem", function(){
+		that.deselectMovingItem();
+	})
+	newFolder.domObj.addEventListener("createWindow", function(){
+		that.createWindow(newFolder);
+	})
 }
 
 Desktop.prototype.createWindow = function(icon){
 	var newWindow = new Window(icon.name, icon);
-	newWindow.bindEvent();
-	newWindow.setPosition(200, 200);	
-
 	this.memberWindow.push(newWindow);
 	this.domObj.appendChild(newWindow.domObj);
+
+	var that = this;
+
+	newWindow.domObj.addEventListener("selectMovingItem", function(){
+		that.selectMovingItem(newWindow);
+	})
+	newWindow.domObj.addEventListener("deselectMovingItem", function(){
+		that.deselectMovingItem();
+	})
 }
 
 Desktop.prototype.selectMovingItem = function(item){
@@ -80,38 +73,35 @@ Desktop.prototype.deselectMovingItem = function(){
 
 var Icon = function(name) {
 	/* TODO: Icon 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-	var domObj = document.createElement('div');
-	domObj.classList.add("icon");
-	domObj.classList.add(name);
-	domObj.innerHTML = name;
-
-	this.domObj = domObj,
+	this.domObj,
 	this.name = name,
 	this.position = [0,0],
 	this.offset = [0,0];
+
+	this.setDom(name);
+	this.bindEvent();
+	this.setPosition(200, 400);
 };
 
 Icon.prototype.bindEvent = function(){
 	var that = this;
 
-	var selectMovingItem = new Event("selectMovingItem", {"bubbles": true});
-	var deselectMovingItem = new Event("deselectMovingItem", {"bubbles": true});
-
-	this.domObj.addEventListener("selectMovingItem", function(){
-		console.log("selectMovingItem on " + that.name);
-	})
-
-	this.domObj.addEventListener("deselectMovingItem", function(){
-		console.log("deselectMovingItem on " + that.name);
-	})
-
 	this.domObj.addEventListener('mousedown', function(event){
 		that.setOffset(event);
-		this.dispatchEvent(selectMovingItem);
+		that.fireEvent("selectMovingItem");
 	})
 	this.domObj.addEventListener('mouseup', function(){
-		this.dispatchEvent(deselectMovingItem);
+		that.fireEvent("deselectMovingItem");
 	})
+}
+
+Icon.prototype.setDom = function(name){
+	var domObj = document.createElement('div');
+	domObj.classList.add("icon");
+	domObj.classList.add(name);
+	domObj.innerHTML = name;
+
+	this.domObj = domObj;
 }
 
 Icon.prototype.setPosition = function(x,y){
@@ -131,68 +121,76 @@ Icon.prototype.movePosition = function(event){
 	this.setPosition(event.clientX - this.offset[0], event.clientY - this.offset[1]);
 }
 
+Icon.prototype.fireEvent = function(eventName){
+	var event = new Event(eventName);
+	this.domObj.dispatchEvent(event);
+}
 
 var Folder = function(name) {
 	/* TODO: Folder 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-	var domObj = document.createElement('div');
-	domObj.classList.add("folder");
-	domObj.classList.add(name);
-	domObj.innerHTML = name;
-
-	this.domObj = domObj,
+	this.domObj,
 	this.name = name,
 	this.position = [0,0],
 	this.offset = [0,0];
+
+	this.setDom(name);
+	this.bindEvent();
+	this.setPosition(400, 200);
 };
 
 Folder.prototype.bindEvent = function(){
 	var that = this;
 
-	var selectMovingItem = new Event("selectMovingItem", {"bubbles": true});
-	var deselectMovingItem = new Event("deselectMovingItem", {"bubbles": true});
-	var createWindow = new Event("createWindow", {"bubbles": true});
-
-	this.domObj.addEventListener("selectMovingItem", function(){
-		console.log("selectMovingItem on " + that.name);
-	})
-
-	this.domObj.addEventListener("deselectMovingItem", function(){
-		console.log("deselectMovingItem on " + that.name);
-	})
-
 	this.domObj.addEventListener('mousedown', function(event){
 		that.setOffset(event);
-		this.dispatchEvent(selectMovingItem);
+		that.fireEvent("selectMovingItem");
 	})
 	this.domObj.addEventListener('mouseup', function(){
-		this.dispatchEvent(deselectMovingItem);
+		that.fireEvent("deselectMovingItem");
 	})
 	this.domObj.addEventListener('dblclick', function(){
-		this.dispatchEvent(createWindow);
+		that.fireEvent("createWindow");
 	})
+}
+
+Folder.prototype.setDom = function(name){
+	var domObj = document.createElement('div');
+	domObj.classList.add("folder");
+	domObj.classList.add(name);
+	domObj.innerHTML = name;
+
+	this.domObj = domObj;
 }
 
 Folder.prototype.setPosition = Icon.prototype.setPosition;
 Folder.prototype.setOffset = Icon.prototype.setOffset;
 Folder.prototype.movePosition = Icon.prototype.movePosition;
-
+Folder.prototype.fireEvent = Icon.prototype.fireEvent;
 
 var Window = function(name, icon) {
 	/* TODO: Window 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-	var domObj = document.createElement('div');
-	domObj.classList.add("window");
-	domObj.classList.add(name);
-	domObj.innerHTML = icon.name;
-
-	this.domObj = domObj,
+	this.domObj,
 	this.position = [0,0],
 	this.offset = [0,0],
 	this.connectedIcon = icon,
 	this.name = icon.name;
+
+	this.setDom(name);
+	this.bindEvent();
+	this.setPosition(400, 400);
 };
 
-Window.prototype.bindEvent = Icon.prototype.bindEvent;
+Window.prototype.setDom = function(name){
+	var domObj = document.createElement('div');
+	domObj.classList.add("window");
+	domObj.classList.add(name);
+	domObj.innerHTML = name;
 
+	this.domObj = domObj;
+}
+
+Window.prototype.bindEvent = Icon.prototype.bindEvent;
 Window.prototype.setPosition = Icon.prototype.setPosition;
 Window.prototype.setOffset = Icon.prototype.setOffset;
 Window.prototype.movePosition = Icon.prototype.movePosition;
+Window.prototype.fireEvent = Icon.prototype.fireEvent;
