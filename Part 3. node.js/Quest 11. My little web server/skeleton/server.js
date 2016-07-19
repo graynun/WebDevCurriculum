c9495f9811553f5dec5ac6e7d6fee09cc7f73675
graynun <graynun@gmail.com>
2016-07-19 14:57:12 +0900

    Quest 11 done

--- Part 3. node.js/Quest 11. My little web server/skeleton/server.js
+++ Part 3. node.js/Quest 11. My little web server/skeleton/server.js
@@ -2,4 +2,29 @@ var http = require('http');
 
 http.createServer(function(req, res) {
 	// TODO: 이 곳을 채워넣으세요..!
+	
+	var barValueFinder = function(reqUrl){
+		var reqStr = reqUrl.split("bar=");
+		return reqStr[reqStr.length - 1];
+	}
+
+	if(req.method === "GET"){
+		if(req.url === "/"){
+			res.end("Hello World!");
+		}else if(req.url.slice(0, 4) === "/foo"){
+			res.end("Hello " + barValueFinder(req.url));
+		}else{
+			console.log("unexpected url " + req.url);
+			res.end();
+		}
+	}else if(req.method === "POST"){
+		if(req.url.slice(0, 4) === "/foo"){
+			res.end("Hello " + barValueFinder(req.url));
+		}else{
+			console.log("unexpected url " + req.url);
+			res.end();
+		}
+	}
+
 }).listen(8080);
+
