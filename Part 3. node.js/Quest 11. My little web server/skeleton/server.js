f3bc29e7f48e1489637e546ea78c362f8f8be818
graynun <graynun@gmail.com>
2016-07-25 16:27:41 +0900

    Quest11 Added process of processing POST request's body

--- Part 3. node.js/Quest 11. My little web server/skeleton/server.js
+++ Part 3. node.js/Quest 11. My little web server/skeleton/server.js
@@ -5,21 +5,14 @@ http.createServer(function(req, res) {
 	
 	var generateKeyAndValueObject = function(reqUrl){
 		var reqStrArr = reqUrl.split(/\?|\&/g);
-		reqStrArr.splice(0,1);
+		if(reqUrl.slice(0, 4) === "/foo") reqStrArr.splice(0,1);
 
 		var keyAndValueObj = {};
 		for(var i=0;i<reqStrArr.length;i++){
 			var keyAndValueArr = reqStrArr[i].split(/\=/);
 			keyAndValueObj[keyAndValueArr[0]] = keyAndValueArr[1];
 		}
-
 		return keyAndValueObj;
-	}
-
-	var generateResponse = function(keyAndValueObj){
-		for (key in keyAndValueObj){
-			res.write("Hello " + keyAndValueObj[key]+"\n");
-		};
 	}
 
 
@@ -28,16 +21,29 @@ http.createServer(function(req, res) {
 			res.end("Hello World!");
 		}else if(req.url.slice(0, 4) === "/foo"){
 			var keyAndValueObj = generateKeyAndValueObject(req.url);
-			if(keyAndValueObj.length != 0) generateResponse(keyAndValueObj);
+			if(keyAndValueObj['bar'] !== undefined) res.write("Hello, "+keyAndValueObj['bar']);
 			res.end();
 		}else{
 			res.end("unexpected url " + req.url);
 		}
 	}else if(req.method === "POST"){
 		if(req.url.slice(0, 4) === "/foo"){
-			var keyAndValueObj = generateKeyAndValueObject(req.url);
-			if(keyAndValueObj.length != 0) generateResponse(keyAndValueObj);
-			res.end();
+			if(req.headers['content-type'] === "application/x-www-form-urlencoded" && req.headers['content-length'] !== '0'){
+				var reqBody;
+				req.on('data', function(chunk){
+					 reqBody = chunk.toString();
+				}).on('end', function(){
+					var keyAndValueObj = generateKeyAndValueObject(reqBody);
+					if(keyAndValueObj['bar'] !== undefined) {
+						res.write("Hello, "+keyAndValueObj['bar']);
+					}else{
+						res.write("no bar on body");
+					};
+					res.end();
+				})
+			}else{
+				res.end("nothing on body");
+			}
 		}else{
 			res.end("unexpected url " + req.url);
 		}
