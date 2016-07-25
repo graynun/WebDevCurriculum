9a8d7b95aaa1e53ef0c8116e28521b11d519bc72
graynun <graynun@gmail.com>
2016-07-25 13:33:36 +0900

    Quest11 modified to support multiple key/values

--- Part 3. node.js/Quest 11. My little web server/skeleton/server.js
+++ Part 3. node.js/Quest 11. My little web server/skeleton/server.js
@@ -3,26 +3,43 @@ var http = require('http');
 http.createServer(function(req, res) {
 	// TODO: 이 곳을 채워넣으세요..!
 	
-	var barValueFinder = function(reqUrl){
-		var reqStr = reqUrl.split("bar=");
-		return reqStr[reqStr.length - 1];
+	var generateKeyAndValueObject = function(reqUrl){
+		var reqStrArr = reqUrl.split(/\?|\&/g);
+		reqStrArr.splice(0,1);
+		
+		var keyAndValueObj = {};
+		for(var i=0;i<reqStrArr.length;i++){
+			var keyAndValueArr = reqStrArr[i].split(/\=/);
+			keyAndValueObj[keyAndValueArr[0]] = keyAndValueArr[1];
+		}
+
+		return keyAndValueObj;
 	}
+
+	var generateResponse = function(keyAndValueObj){
+		for (key in keyAndValueObj){
+			res.write("Hello " + keyAndValueObj[key]+"\n");
+		};
+	}
+
 
 	if(req.method === "GET"){
 		if(req.url === "/"){
 			res.end("Hello World!");
 		}else if(req.url.slice(0, 4) === "/foo"){
-			res.end("Hello " + barValueFinder(req.url));
-		}else{
-			console.log("unexpected url " + req.url);
+			var keyAndValueObj = generateKeyAndValueObject(req.url);
+			generateResponse(keyAndValueObj);
 			res.end();
+		}else{
+			res.end("unexpected url " + req.url);
 		}
 	}else if(req.method === "POST"){
 		if(req.url.slice(0, 4) === "/foo"){
-			res.end("Hello " + barValueFinder(req.url));
-		}else{
-			console.log("unexpected url " + req.url);
+			var keyAndValueObj = generateKeyAndValueObject(req.url);
+			generateResponse(keyAndValueObj);
 			res.end();
+		}else{
+			res.end("unexpected url " + req.url);
 		}
 	}
 
