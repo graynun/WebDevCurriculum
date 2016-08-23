83128cf37fbad744129699e66d1a92c820cccdfc
graynun <graynun@gmail.com>
2016-08-23 20:15:18 +0900

    Quest 15 setup mysql localhost server and testing sequelize

--- Part 4. Database/Quest 15. ORM/skeleton/client/notepad.js
+++ Part 4. Database/Quest 15. ORM/skeleton/client/notepad.js
@@ -1,3 +1,418 @@
+"use strict";
+
 var Notepad = function() {
 	/* TODO: 그 외에 또 어떤 클래스와 메소드가 정의되어야 할까요? */
+	this.dom = document.querySelector(".notepad");
+	this.noteArea = new NoteArea(),
+	this.fileList = new FileList(),
+	this.fileTab = new FileTab(),
+	this.fileArr = {},
+	this.selectedFile = undefined;
+
+	this.mapEvents();
+	this.loadFiles();
+	this.loadLastStatus();
 };
+
+Notepad.prototype.loadLastStatus = function(){
+	var that = this;
+
+	XMLHTTPGetRequestMaker(that, "http://localhost:8080/loadLastStatus", function(that, responseText){
+		var lastStatus = JSON.parse(responseText);
+
+		document.querySelector(".fileListArea>h1").innerHTML = lastStatus.user+"'s file list";
+
+		if(lastStatus.lastTabs !== null){
+			for(var i=0;i<lastStatus.lastTabs.length;i++){
+				that.fileTab.createTab(that.fileArr[lastStatus.lastTabs[i]]);
+			}
+
+			var selectFile = new CustomEvent('selectFile', {
+				detail: that.fileArr[lastStatus.lastSelected],
+				bubbles: true
+			});
+			that.dom.dispatchEvent(selectFile);			
+		}
+	})
+}
+
+Notepad.prototype.mapEvents = function(){
+	var that = this;
+
+	document.querySelector(".saveFile").onclick = function(){
+		that.saveFile();	
+	}
+
+	document.querySelector(".newFile").onclick = function(){
+		var deselectFile = new Event('deselectFile');
+		that.dom.dispatchEvent(deselectFile);
+	}
+
+	document.querySelector(".logout").onclick = function(){
+		console.log("Ever called?");
+
+		var currentTabs = [];
+		for(var i=0;i<that.fileTab.fileTabArr.length;i++){
+			currentTabs.push(that.fileTab.fileTabArr[i].fileName);
+		}
+		var sendInfo = {};
+		sendInfo["tabs"] = currentTabs.length === 0? null : currentTabs; 
+		sendInfo["selected"] = that.selectedFile === undefined? null : that.selectedFile.fileName;
+
+		XMLHTTPPostRequestMaker(that, "http://localhost:8080/logout", sendInfo, function(that, responseURL){
+			window.location.replace(responseURL);
+		});
+	}
+
+	this.dom.addEventListener('deselectFile', function(){
+		that.fileList.deselectFile();
+		that.fileTab.deselectFile();
+		that.deselectFile();
+
+		that.noteArea.emptyContentArea();
+	})
+
+	this.dom.addEventListener('selectFile', function(event){
+		that.selectedFile = event.detail;
+		that.noteArea.showFileContent(that.selectedFile);
+		that.fileList.selectFile(that.selectedFile);
+		that.fileTab.selectFile(that.selectedFile);
+	})
+
+	this.dom.addEventListener('removeTab', function(event){
+		that.fileTab.removeTab(event.detail);
+	})
+}
+
+Notepad.prototype.loadFiles = function(){
+	XMLHTTPGetRequestMaker(this, 'http://localhost:8080/reloadFileList', this.appendList);
+}
+
+Notepad.prototype.appendList = function(subject, list){
+	var fileNameList = JSON.parse(list);
+	
+	for(var i=0;i<fileNameList.length;i++){
+		if(subject.fileArr[fileNameList[i]] === undefined){
+			var myFile = new NotepadItem(fileNameList[i]);
+			subject.fileArr[myFile.fileName]= myFile;
+		}
+	}	
+
+	subject.fileList.emptyList();
+	subject.fileList.reloadList(subject.fileArr);
+
+	if(subject.selectedFile !== undefined){
+		var selectFile = new CustomEvent('selectFile', {
+			detail: subject.selectedFile
+		});
+		subject.dom.dispatchEvent(selectFile);
+	}
+}
+
+Notepad.prototype.selectFile = function(notepadItem){
+	this.selectedFile = notepadItem;
+}
+
+Notepad.prototype.deselectFile = function(){
+	this.selectedFile = undefined;
+}
+
+Notepad.prototype.saveFile = function(){
+	var that = this;
+	
+	var content;
+
+	if(this.selectedFile === undefined){
+		if(this.fileList.fileListArr.length === 0){
+			content ={
+				title: "file1" ,
+				content: document.querySelector(".fileBody").value	
+			};
+			var myFile = new NotepadItem("file1");
+		}else{
+			content = {
+				title: "file" + Number(that.fileList.fileListArr.length+1),
+				content: document.querySelector(".fileBody").value
+			}
+			var myFile = new NotepadItem("file" + Number(that.fileList.fileListArr.length+1));
+		}
+
+		that.fileArr[myFile.fileName] = myFile;
+		this.selectFile(myFile);
+
+	}else{
+		content = {
+			title: this.selectedFile.fileName,
+			content: document.querySelector(".fileBody").value
+		};
+	}
+
+	XMLHTTPPostRequestMaker(this, 'http://localhost:8080/savefile', content, this.reloadListAndSelect);
+
+}
+
+Notepad.prototype.reloadListAndSelect = function(subject){
+	console.log("safely saved on fs");
+	subject.loadFiles();
+	var selectHighLighter = new Event('selectedFile');
+	document.dispatchEvent(selectHighLighter);
+}
+
+
+var NotepadItem = function(fileName){
+	this.fileName = fileName,
+	this.tabExist = false,
+	this.fileListExist = false;
+}
+
+var FileList = function(){
+	this.fileListDom = document.querySelector(".fileList"),
+	this.fileListArr = [];
+}
+
+FileList.prototype.reloadList = function(fileArr){
+	var that = this;	
+	for(var key in fileArr){
+		var fileListItem = new FileListItem(fileArr[key]);
+		this.fileListArr.push(fileListItem);
+		this.fileListDom.appendChild(fileListItem.dom);
+	}
+}
+
+FileList.prototype.emptyList = function(){
+	this.fileListArr = [];
+	while(this.fileListDom.firstChild){
+		this.fileListDom.removeChild(this.fileListDom.firstChild);
+	}
+}
+
+FileList.prototype.selectFile = function(selectedFile){
+	for(var i=0;i<this.fileListArr.length;i++){
+		if(selectedFile === this.fileListArr[i].notepadItem) this.fileListArr[i].beSelected();
+	}
+}
+
+FileList.prototype.deselectFile = function(){
+	for(var i=0;i<this.fileListArr.length;i++){
+		if(this.fileListArr[i].selected === true) {
+			this.fileListArr[i].beDeselected();
+		}
+	}
+}
+
+var FileListItem = function(notepadItem){
+	this.dom,
+	this.notepadItem = notepadItem,
+	this.fileName = notepadItem.fileName,
+	this.selected = false;
+
+	this.createDom();
+	this.mapDomEvent();
+}
+
+FileListItem.prototype.createDom = function(){
+	var dom = document.createElement('li');
+	dom.innerHTML = this.fileName;
+	dom.classList.add(this.fileName);
+	this.dom = dom;
+	this.notepadItem.fileListExist = true;
+}
+
+FileListItem.prototype.mapDomEvent = function() {
+	var that = this;
+
+	this.dom.onclick = function(){
+		var deselectFile = new Event('deselectFile', {"bubbles":true});
+		that.dom.dispatchEvent(deselectFile);
+
+		var selectFile = new CustomEvent('selectFile', {
+			detail: that.notepadItem,
+			bubbles: true
+		});
+
+		that.dom.dispatchEvent(selectFile);
+	};
+};
+
+FileListItem.prototype.beSelected = function(){
+	this.selected = true;
+	this.dom.classList.add('selected');
+}
+
+FileListItem.prototype.beDeselected = function(){
+	this.selected = false;
+	this.dom.classList.remove('selected');
+}
+
+
+var FileTab = function(){
+	this.dom = document.querySelector(".fileTabContainer"),
+	this.fileTabArr = [];
+}
+
+FileTab.prototype.createTab = function(selectedFile){
+	var fileTabItem = new FileTabItem(selectedFile);
+	this.fileTabArr.push(fileTabItem);
+	this.dom.appendChild(fileTabItem.dom);
+}
+
+FileTab.prototype.selectFile = function(selectedFile){
+	if(selectedFile.tabExist === false) {
+		this.createTab(selectedFile);
+		this.selectFile(selectedFile);	
+	}else{
+		for(var i=0;i<this.fileTabArr.length;i++){		
+			if(selectedFile === this.fileTabArr[i].notepadItem) {
+				this.fileTabArr[i].beSelected();
+			}
+		}
+	}
+}
+
+FileTab.prototype.deselectFile = function(){
+	for(var i=0;i<this.fileTabArr.length;i++){
+		if(this.fileTabArr[i].selected === true) {
+			this.fileTabArr[i].beDeselected();
+		}
+	}
+}
+
+FileTab.prototype.removeTab = function(notepadItem){
+	notepadItem.tabExist = false;
+
+	for(var i=0;i<this.fileTabArr.length;i++){
+		if(this.fileTabArr[i].fileName === notepadItem.fileName) {
+			this.dom.removeChild(this.fileTabArr[i].dom);
+			this.fileTabArr.splice(i, 1);
+		}
+	}
+}
+
+
+var FileTabItem = function(selectedFile){
+	this.dom,
+	this.closeDom,
+	this.notepadItem = selectedFile,
+	this.fileName = selectedFile.fileName,
+	this.selected = false;
+
+	this.createDom();
+}
+
+FileTabItem.prototype.createDom = function(){
+	var dom = document.createElement('div');
+	dom.classList.add(this.fileName);
+	dom.innerHTML = this.fileName;
+	this.dom = dom;
+
+	var closeDom = document.createElement('span');
+	closeDom.classList.add("close");
+	closeDom.innerHTML = "X";
+	this.closeDom = closeDom;
+	this.dom.appendChild(this.closeDom);
+
+	this.notepadItem.tabExist = true;
+
+	this.mapDomEvent();
+}
+
+FileTabItem.prototype.mapDomEvent = function() {
+	var that = this;
+
+	this.dom.onclick = function(){
+		var deselectFile = new Event('deselectFile', {"bubbles":true});
+		that.dom.dispatchEvent(deselectFile);
+
+		var selectFile = new CustomEvent('selectFile', {
+			bubbles: true,
+			detail: that.notepadItem
+		});
+		that.dom.dispatchEvent(selectFile);
+	};
+
+	this.closeDom.addEventListener('click', function(event){
+		if(that.selected === true){
+			var deselectFile = new Event('deselectFile', {"bubbles":true});
+			that.dom.dispatchEvent(deselectFile);
+		}
+		
+		var removeTab = new CustomEvent('removeTab', {
+			bubbles: true,
+			detail: that.notepadItem
+		});
+		that.dom.dispatchEvent(removeTab);
+	});
+};
+
+FileTabItem.prototype.beSelected = function(){
+	this.selected = true;
+	this.dom.classList.add('selected');
+}
+
+FileTabItem.prototype.beDeselected = function(){
+	this.selected = false;
+	this.dom.classList.remove('selected');
+}
+
+
+var NoteArea = function(){
+	this.textareaDom = document.querySelector(".fileBody");
+}
+
+NoteArea.prototype.showFileContent = function(selectedFile){
+	document.querySelector(".fileName").innerHTML = "File name: " + selectedFile.fileName;
+	var req = XMLHTTPGetRequestMaker(this, "http://localhost:8080/readFile?fileName=", this.showContent, selectedFile.fileName);
+}
+
+NoteArea.prototype.emptyContentArea = function(){
+	document.querySelector(".fileName").innerHTML = "File name here";
+	this.textareaDom.value = "";
+}
+
+NoteArea.prototype.showContent = function(subject, content){
+	subject.textareaDom.value = content;
+}
+
+
+
+
+var XMLHTTPGetRequestMaker = function(subject, url, callback, fileName){
+	// subject를 받지 않고 callback을 우아하게 부를 수 있는 방법은 없나?
+
+	var req = new XMLHttpRequest();
+
+	req.onreadystatechange = function(){
+		if(req.readyState === XMLHttpRequest.DONE){ 
+			if(req.status === 200){
+				callback(subject, req.responseText);
+			}else{
+				console.log("req status is not 200");
+			}
+		}
+	};
+
+	url = fileName === undefined? url : url+fileName;
+	req.open('GET', url);
+	req.send();
+}
+
+var XMLHTTPPostRequestMaker = function(subject, url, content, callback){
+// subject를 받지 않고 callback을 우아하게 부를 수 있는 방법은 없나?
+
+	var req = new XMLHttpRequest();
+
+	req.onreadystatechange = function(){
+		if(req.readyState === XMLHttpRequest.DONE){
+			if(req.status === 200){
+				callback(subject, req.responseURL);
+			}else{
+				console.log("req status is not 200");
+			}
+		}
+	}
+
+	req.open('POST', url);
+	req.setRequestHeader('Content-type', 'application/json;charset=utf-8');
+	req.send(JSON.stringify(content));	
+};
+
--- Part 4. Database/Quest 15. ORM/skeleton/server.js
+++ Part 4. Database/Quest 15. ORM/skeleton/server.js
@@ -1,16 +1,186 @@
+"use strict";
+
 var express = require('express'),
-	Sequelize = require('sequelize'),
 	path = require('path'),
-	app = express();
+	bodyParser = require('body-parser'),
+	session = require('express-session'),
+	fs = require('fs'),
+	app = express(),
+	fileManager = new FileManager(),
+	userManager = new UserManager();
 
 app.use(express.static('client'));
+app.use(bodyParser.json());
+app.use(
+	session({
+		secret: 'change string whenvever I want to',
+		resave: false,
+		saveUninitialized: false,
+		cookie: {
+			secure: false,
+			maxAge: 2 * 60 * 1000
+		}
+	})
+);
 
-app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, 'index.html'));
+
+app.use('/', function(req, res, next){
+	console.log("request session user at / is "+ req.session.user);
+	console.log("app.use request sessionID is "+req.sessionID);
+	next();
 });
+
+app.get('/login', function(req, res){
+	console.log("app.get request sessionID at /login is "+req.sessionID);
+	console.log(req.session);
+	res.sendFile(path.join(__dirname, '/client/login.html'));
+})
+
+
+app.post('/loginClicked', function(req, res){
+	console.log("get loginclicked got req.query? in get?");
+	
+	console.log(req.body.id);
+	console.log(req.body.pw)
+
+	var loginSuccess = userManager.authenticate(req.body.id, req.body.pw);
+
+	if(loginSuccess[0]){
+		req.session.save(function(err){
+			req.session.user = req.body.id;
+			console.log("signed in user is "+req.session.user);
+			res.redirect('/main');			
+		})
+	}else{
+		console.log("login failed with "+loginSuccess[1]);
+		res.status(401).send(loginSuccess[1]);
+	}
+});
+
+app.post('/logout', function(req, res){
+	console.log("Ever called /logout?");
+	console.log(req.body.tabs);
+	console.log(req.body.selected);
+	userManager.saveLastStatus(req.session.user, req.body.tabs, req.body.selected);
+	req.session.destroy(function(err){
+		if(err) throw err;
+		res.redirect('/login');
+	})
+	
+});
+
+
+app.get('/main', function (req, res) {
+	console.log("got /main get");
+	res.sendFile(path.join(__dirname, '/client/index.html'));
+});
+
+
 
 /* TODO: 여기에 처리해야 할 요청의 주소별로 동작을 채워넣어 보세요..! */
 
 var server = app.listen(8080, function () {
-	console.log('Server started!');
+	console.log('Server started! at 8080');
 });
+
+app.post('/savefile', function(req, res){
+	console.log("savefile had called");
+
+	fileManager.createFile(req.session.user, req.body);
+	res.end();
+});
+
+app.get('/reloadFileList', function(req, res){
+	console.log("got reloadFileList req?");
+	console.log(req.session);
+	var fileArr = fileManager.readFileList(req.session.user);
+	res.send(fileArr);
+	console.log(fileArr);
+	res.end();
+});
+
+app.get('/readFile', function(req, res){
+	console.log(req.query.fileName);
+	var data = fileManager.readFile(req.session.user, req.query.fileName);
+	res.send(data);
+	res.end();
+})
+
+app.get('/loadLastStatus', function(req, res){
+	var responseContent = userManager.readLastStatus(req.session.user);
+	res.send(responseContent);
+	res.end();
+})
+
+
+function FileManager() {
+	this.dir;
+};
+
+FileManager.prototype.createFile = function(userID, reqBody){
+	this.dir = "./"+userID+"/";
+	fs.writeFile(this.dir+reqBody.title+'.txt', reqBody.content,
+		function(err){
+			if(err) throw err;
+			console.log("file "+reqBody.title+" created!");
+		}
+	);
+}
+
+FileManager.prototype.readFileList = function(userID){
+	this.dir = "./"+userID+"/";
+	var fileNameArr = fs.readdirSync(this.dir);
+	if(fileNameArr[0] === ".DS_Store") fileNameArr.splice(0,1);
+	for(var i=0;i<fileNameArr.length;i++){
+		fileNameArr[i] = fileNameArr[i].split(/.txt$/)[0];
+	}
+	return fileNameArr;
+}
+
+FileManager.prototype.readFile = function(userID, fileName){
+	this.dir = "./"+userID+"/";
+	console.log(this.dir);
+	console.log(this.dir+fileName+".txt");
+	return fs.readFileSync(this.dir+fileName+".txt", 'utf8');
+}
+
+
+function UserManager(){
+	this.userInfo = require('./userInfo.json');
+	console.log(this.userInfo);
+}
+
+
+UserManager.prototype.authenticate = function(id, pw){
+	if(this.userInfo[id] === undefined){
+		console.log("there's no such user");
+		return [false, "No such user"];
+	}else{
+		if(this.userInfo[id]['pw'] === pw){
+			console.log("yay user authenticated!");
+			return [true, this.userInfo[id]["lastTabs"],this.userInfo[id]["lastSelected"]];
+		}else{
+			return [false, "wrong password"];
+		}
+	}
+}
+
+UserManager.prototype.saveLastStatus = function(id, tabs, selected){
+	console.log(tabs);
+	console.log(selected);
+	this.userInfo[id]["lastTabs"] = tabs;
+	this.userInfo[id]["lastSelected"] = selected;
+	console.log(this.userInfo);
+	fs.writeFile('./userInfo.json', JSON.stringify(this.userInfo), function(err){
+		if(err) throw err;
+		console.log("updated lastStatus to the json file");
+	});
+}
+
+UserManager.prototype.readLastStatus = function(id){
+	var obj = {};
+	obj["user"] = this.userInfo[id]['id'];
+	obj["lastTabs"] = this.userInfo[id]["lastTabs"];
+	obj["lastSelected"] = this.userInfo[id]["lastSelected"];
+	return obj;
+}
