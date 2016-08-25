8d5344496c34f8ca510e30324897d5be45b83b89
graynun <graynun@gmail.com>
2016-08-25 19:21:04 +0900

    Quest15 need to implement getter/setter for models

--- Part 4. Database/Quest 15. ORM/sequelizeTest/sequelize_test.js
+++ Part 4. Database/Quest 15. ORM/sequelizeTest/sequelize_test.js
@@ -48,6 +48,10 @@ var Notes = sequelize.define('Notes', {
 		field: 'author',
 		allowNull: false
 	},
+	notename: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
 	content: {
 		type: Sequelize.TEXT,
 		field: 'content',
@@ -95,17 +99,22 @@ var Lastopened = sequelize.define('Lastopened', {
 Users.sync({force:true}).then(function(){
 	console.log("Users table created!");
 	// Create pre-exsiting users into Users table
-
-	fs.readdir('./notes', function(err, files){
-		for(var i=0;i<files.length;i++){
-			if(files[i] !== '.DS_Store'){
-				Users.create({
-					user_id: files[i],
-					hashed_password: "default"
-				});
-			}
-		}
+	return new Promise(function(resolve, reject){
+		fs.readdir('./notes', function(err, files){
+			if(err !== null) return reject(err);
+			resolve(files);
+		});
 	});
+
+}).then(function(files){
+	for(var i=0;i<files.length;i++){
+		if(files[i] !== '.DS_Store'){
+			Users.create({
+				user_id: files[i],
+				hashed_password: "default"
+			});
+		}
+	}	
 }).then(function(){
 	Notes.sync({force:true}).then(function(){
 		console.log("Notes table created!");
@@ -115,22 +124,26 @@ Users.sync({force:true}).then(function(){
 		}).then(function(userInfoArr){
 			userInfoArr.forEach(function(user){
 				fs.readdir('./notes/'+user.user_id, function(err, noteList){
+					if(err !== null) throw err;
 					noteList.forEach(function(notename){
 						if(notename !== '.DS_Store'){
 							fs.readFile('./notes/'+user.user_id+'/'+notename, 'utf8', function(err, data){
-								// console.log(data);
 								Notes.create({
 									author: user.id,
+									notename: notename.split(/\.txt/)[0],
 									content: data.toString()
 								})
 							});
 						}
-					})
+					});
 				});
-			})
+			});
 		}).catch(function(error){
 			console.log("oops error "+ error.message);
 		});
+
+	}).catch(function(error){
+		console.log("oops error "+ error.message);
 	});
 });
 
--- Part 4. Database/Quest 15. ORM/skeleton/server.js
+++ Part 4. Database/Quest 15. ORM/skeleton/server.js
@@ -4,10 +4,13 @@ var express = require('express'),
 	path = require('path'),
 	bodyParser = require('body-parser'),
 	session = require('express-session'),
+	mysql = require('mysql'),
+	Sequelize = require('sequelize'),
+	dbaccessinfo = require('../sequelizeTest/dbaccess.js'),
 	fs = require('fs'),
 	app = express(),
 	fileManager = new FileManager(),
-	userManager = new UserManager();
+	dbManager = new DBManager();
 
 app.use(express.static('client'));
 app.use(bodyParser.json());
@@ -22,6 +25,103 @@ app.use(
 		}
 	})
 );
+
+var sequelize = new Sequelize(dbaccessinfo.dbname, dbaccessinfo.account, dbaccessinfo.password, {
+	host: 'localhost',
+	dialect: 'mysql'
+});
+
+var Users = sequelize.define('Users', {
+	user_id: {
+		type: Sequelize.STRING,
+		field: 'user_id',
+		allowNull: false,
+	},
+	hashed_password: {
+		type: Sequelize.STRING,
+		field: 'hashed_password',
+		allowNull: false
+	},
+	nickname: {
+		type: Sequelize.STRING,
+	}
+}, {
+	freezeTableName: true,
+	underscored: true,
+	charset: 'utf8'
+});
+
+Users.sync();
+
+Users.findAll().then(function(userrows){
+	userrows.forEach(function(userrow){
+		console.log(userrow.user_id);
+	})
+	
+}).catch(function(err){
+	console.log(err);
+});
+
+var Notes = sequelize.define('Notes', {
+	note_id: {
+		type: Sequelize.INTEGER(255).UNSIGNED,
+		field: 'note_id',
+		allowNull: false,
+		autoIncrement: true,
+		primaryKey: true
+	},
+	author: {
+		type: Sequelize.INTEGER.UNSIGNED,
+		field: 'author',
+		allowNull: false
+	},
+	notename: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	content: {
+		type: Sequelize.TEXT,
+		field: 'content',
+		allowNull: false
+	}
+}, {
+	timestamps: true,
+	paranoid: true,
+	freezeTableName: true,
+	underscored: true,
+	charset: 'utf8'
+})
+
+Notes.sync();
+
+
+var Lastopened = sequelize.define('Lastopened', {
+	note_id: {
+		type: Sequelize.INTEGER.UNSIGNED,
+		field: 'note_id',
+		allowNull: false
+	},
+	author: {
+		type: Sequelize.INTEGER.UNSIGNED,
+		field: 'author',
+		allowNull: false
+	},
+	selected: {
+		type: Sequelize.BOOLEAN,
+		field: 'selected',
+		allowNull: false
+	}
+}, {
+	timestamps: true,
+	paranoid: true,
+	freezeTableName: true,
+	underscored: true,
+	charset: 'utf8'
+})
+
+Lastopened.sync();
+
+
 
 
 app.use('/', function(req, res, next){
@@ -39,29 +139,37 @@ app.get('/login', function(req, res){
 
 app.post('/loginClicked', function(req, res){
 	console.log("get loginclicked got req.query? in get?");
-	
 	console.log(req.body.id);
 	console.log(req.body.pw)
 
-	var loginSuccess = userManager.authenticate(req.body.id, req.body.pw);
-
-	if(loginSuccess[0]){
-		req.session.save(function(err){
-			req.session.user = req.body.id;
-			console.log("signed in user is "+req.session.user);
-			res.redirect('/main');			
-		})
-	}else{
-		console.log("login failed with "+loginSuccess[1]);
-		res.status(401).send(loginSuccess[1]);
-	}
+	Users.findOne({
+		attributes: ['id', 'user_id', 'hashed_password'],
+		where:{
+			'user_id': req.body.id
+		}
+	}).then(function(userInstance){
+		if(userInstance === null) {
+			res.status(401).send("no such user");
+		}else if(userInstance.hashed_password === req.body.pw){
+			req.session.save(function(err){
+				req.session.user_id = req.body.id;
+				req.session.user = userInstance.id;
+				console.log("signed in user is "+req.session.user);
+				res.redirect('/main');
+			});			
+		}else{
+			res.status(401).send("wrong password");
+		}
+	}).catch(function(err){
+		console.log(err);
+	});
 });
 
 app.post('/logout', function(req, res){
 	console.log("Ever called /logout?");
 	console.log(req.body.tabs);
 	console.log(req.body.selected);
-	userManager.saveLastStatus(req.session.user, req.body.tabs, req.body.selected);
+	// DBManager.saveLastStatus(req.session.user, req.body.tabs, req.body.selected);
 	req.session.destroy(function(err){
 		if(err) throw err;
 		res.redirect('/login');
@@ -93,10 +201,24 @@ app.post('/savefile', function(req, res){
 app.get('/reloadFileList', function(req, res){
 	console.log("got reloadFileList req?");
 	console.log(req.session);
-	var fileArr = fileManager.readFileList(req.session.user);
-	res.send(fileArr);
-	console.log(fileArr);
-	res.end();
+	// var fileArr = fileManager.readFileList(req.session.user);
+
+	Notes.findAll({
+		attributes: ['notename'],
+		where:{
+			'author': req.session.user
+		}
+	}).then(function(notenameArr){
+		var obj = [];
+		notenameArr.forEach(function(filename){
+			obj.push(filename.notename);
+		})
+		res.send(obj);
+		console.log(obj);
+		res.end();
+	})
+
+
 });
 
 app.get('/readFile', function(req, res){
@@ -107,9 +229,52 @@ app.get('/readFile', function(req, res){
 })
 
 app.get('/loadLastStatus', function(req, res){
-	var responseContent = userManager.readLastStatus(req.session.user);
-	res.send(responseContent);
-	res.end();
+	var obj = {};
+	// obj["user"] = this.userInfo[id]['id'];
+	// obj["lastTabs"] = this.userInfo[id]["lastTabs"];
+	// obj["lastSelected"] = this.userInfo[id]["lastSelected"];
+	// return obj;
+
+	// var responseContent = DBManager.readLastStatus(req.session.user);
+	// res.send(responseContent);
+	// res.end();
+	
+	Lastopened.findAll({
+		attributes: ['note_id', 'selected'],
+		where:{
+			'author': req.session.user
+		}
+	}).then(function(lastopenedrows){
+		obj["user"] = req.session.user_id;
+		// console.log(lastopenedrows);
+
+		var lastTabs = []
+		lastopenedrows.forEach(function(eachrow){
+			Notes.findOne({
+				attributes: ['notename'],
+				where: {
+					'note_id':eachrow.note_id
+				}
+			}).then(function(notenamerow){
+				console.log(notenamerow[0].notename);
+				lastTabs.push(notenamerow[0].notename);
+				if(eachrow.selected === true) obj["lastSelected"] = notenamerow.notename;	
+			});
+		})
+
+
+		// var lastTabs = [];
+		// lastopenedrows.forEach(function(eachrow){
+		// 	console.log(eachrow);
+		// 	console.log(eachrow.notename);
+		// 	lastTabs.push(eachrow.notename);
+		// 	if(eachrow.selected === true) obj["lastSelected"] = eachrow.notename;
+		// })
+		// obj["lastTabs"] = lastTabs;
+
+		res.send(obj);
+		res.end();
+	})
 })
 
 
@@ -145,27 +310,28 @@ FileManager.prototype.readFile = function(userID, fileName){
 }
 
 
-function UserManager(){
-	this.userInfo = require('./userInfo.json');
-	console.log(this.userInfo);
+function DBManager(){
+	// this.userInfo = require('./userInfo.json');
+	// console.log(this.userInfo);
 }
 
 
-UserManager.prototype.authenticate = function(id, pw){
-	if(this.userInfo[id] === undefined){
-		console.log("there's no such user");
-		return [false, "No such user"];
-	}else{
-		if(this.userInfo[id]['pw'] === pw){
-			console.log("yay user authenticated!");
-			return [true, this.userInfo[id]["lastTabs"],this.userInfo[id]["lastSelected"]];
-		}else{
-			return [false, "wrong password"];
-		}
-	}
+DBManager.prototype.authenticate = function(id, pw){
+
+	// if(this.userInfo[id] === undefined){
+	// 	console.log("there's no such user");
+	// 	return [false, "No such user"];
+	// }else{
+	// 	if(this.userInfo[id]['pw'] === pw){
+	// 		console.log("yay user authenticated!");
+	// 		return [true, this.userInfo[id]["lastTabs"],this.userInfo[id]["lastSelected"]];
+	// 	}else{
+	// 		return [false, "wrong password"];
+	// 	}
+	// }
 }
 
-UserManager.prototype.saveLastStatus = function(id, tabs, selected){
+DBManager.prototype.saveLastStatus = function(id, tabs, selected){
 	console.log(tabs);
 	console.log(selected);
 	this.userInfo[id]["lastTabs"] = tabs;
@@ -177,7 +343,7 @@ UserManager.prototype.saveLastStatus = function(id, tabs, selected){
 	});
 }
 
-UserManager.prototype.readLastStatus = function(id){
+DBManager.prototype.readLastStatus = function(id){
 	var obj = {};
 	obj["user"] = this.userInfo[id]['id'];
 	obj["lastTabs"] = this.userInfo[id]["lastTabs"];
