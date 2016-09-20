f470214ea76ee86922f76f1c13dc038c191b2795
graynun <graynun@gmail.com>
2016-09-20 11:32:39 +0900

    Quest18 done

--- Part 5. Modern web/Quest 18. WebSocket/submit/client/sketchio.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-let socket = io('http://localhost:8080');
-
-// socket.on('testemit', (data) => {
-// 	console.log("Does it reach to here?");
-// 	console.log(data.roomname);
-// })
