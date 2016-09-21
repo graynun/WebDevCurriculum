b56b0501a532852b8f1fb395fb41d0fff64c753f
graynun <graynun@gmail.com>
2016-09-21 16:49:32 +0900

    Quest19 letting server to forward access token info to server

--- Part 5. Modern web/Quest 19. OAuth/oauthtest/server.js
+++ Part 5. Modern web/Quest 19. OAuth/oauthtest/server.js
@@ -5,8 +5,8 @@ const express = require('express'),
 let idtoken;
 
 app.use(express.static(__dirname));
-// app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+// app.use(bodyParser.json());
 
 app.listen(8080, (err)=>{
 	if(err) throw err;
@@ -17,7 +17,14 @@ app.get('/', (req, res)=> {
 	 res.sendFile(__dirname+'/login.html');
 })
 
+app.post('/login', (req, res)=> {
+	console.log("************************************* login post body *****************************");
+	console.log(req.body);
+	res.redirect('/main');
+})
+
 app.get('/main', (req, res)=>{
+	// console.log(req);
 	res.sendFile(__dirname+'/main.html');
 });
 
